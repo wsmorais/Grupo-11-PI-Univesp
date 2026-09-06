4999bc20a59e1d7d5b337549049e01f71c8338d8
wsmorais <wilson1first@gmail.com>
2026-09-06 14:39:28 -0300

    Ajusta STATIC_ROOT e adiciona Whitenoise no settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Adicione esta linha
 ]
 
 ROOT_URLCONF = 'setup.urls'
@@ -126,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Email
